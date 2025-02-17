69dc3da76c3957248ba9c05985f3fc37f53a2293
andreevaiva <i.andreeva26@acsbg.org>
2025-02-17 14:33:09 +0200

    Update code.java

--- code.java
+++ code.java
@@ -6,31 +6,38 @@ public class Main
         String[] names = {"Kinder", "Corny", "FitSpo", "Water", "Iced Tea"};
         double[] prices = {2.20, 1.70, 4.80, 1.20, 2.30};
         int[] quantity = {5, 7, 10, 15, 6};
-	    System.out.print("Enter'exit' to exit the program.\n\n");
+	    System.out.print("Enter '-1' to exit the program.\n\n");
 	    
 	    displayAvailability(names, prices, quantity);
+	    System.out.print("\nEnter code: ");
 	    
+	    int code=scanner.nextInt();
 	    do{
-	        int code = scanner.nextInt();
 	        if(code>=0 && code<quantity.length){
 	            if(quantity[code]==0){
 	                System.out.println("We do not have this product!");
-	                break;
-	            }
-	            System.out.print("Please enter "+prices[ code] +" leva: ");
-	            double inserted = scanner.nextDouble();
-	            if(inserted>=prices[code]){
-	                System.out.print("Here you go. This is your change:"+ (inserted-prices[code])+"\n\n");
-	                quantity[code]--;
 	            }else{
-	                System.out.print("Not enough money!\n\n");
+	                System.out.print("Please enter money: ");
+	                double inserted = scanner.nextDouble();
+	                if(inserted>=prices[code]){
+	                    System.out.print("Here you go. This is your change:"+ (inserted-prices[code])+"\n\n");
+	                    quantity[code]--;
+	                }else{
+	                    System.out.print("Not enough money!\n\n");
+	                }
 	            }
 	        }else{
+	            if(code==-1){
+	                System.out.print("arr");
+	                break;
+	            }
 	            System.out.print("Invalid code!\n\n");
 	        }
-	    }while(scanner.nextLine()!="exit");
+	        System.out.print("Enter code: ");
+	        code = scanner.nextInt();
+	    }while(code!=-1);
 	    
-	    System.out.print("Enter code: ");
+	    
 	}
 	private static void displayAvailability(String[] names, double[] prices, int[] quantity){
 	    for(int i=0; i<quantity.length; i++){
